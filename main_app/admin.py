871f652dbf72f463727930183ce12b4c491ca7f1
Andrii Tomalia <logotip123@yahoo.com>
2020-01-20 12:10:59 +0200

    adding slug autofill

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import Categories, Product
 
 
-admin.site.register(Categories)
-admin.site.register(Product)
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    fields = ('name', 'description')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+    list_display = ('title', 'relevant', 'category')
--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,21 @@
+import datetime
+
 from django.db import models
+from django.utils.text import slugify
+
 from subscription_service.settings import PROJECT_VARIABLES
 
 
 class Categories(models.Model):
     name = models.CharField(unique=True, max_length=PROJECT_VARIABLES["category_name_max_length"])
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, null=True)
     created = models.DateField(auto_now_add=True)
     description = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -19,6 +28,12 @@ class Product(models.Model):
     relevant = models.DateTimeField(verbose_name="Relevant until")
     description = models.TextField()
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            date = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            self.slug = slugify(str(self.title) + date)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
