2484b1e37ce805af7f8a649d72505c462b7c61d5
Andrii Tomalia <logotip123@yahoo.com>
2020-01-20 14:38:13 +0200

    adding subscribe and unsubscribe logic

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from .models import Categories, Product
+from users.models import UserCabinet
 
 
 def get_index(request):
@@ -12,8 +13,19 @@ def get_index(request):
 
 def get_categories(request):
     categories = Categories.objects.all()
+    user_cabinet = UserCabinet.objects.filter(user__username=request.user.username).first()
+    if request.POST:
+        if 'subscribe' in request.POST:
+            subscribe = Categories.objects.filter(name=request.POST['subscribe']).first()
+            user_cabinet.subscriptions.add(subscribe)
+            messages.success(request, f"Subscribe to {subscribe.name} was successful")
+        elif 'unsubscribe' in request.POST:
+            subscribe = Categories.objects.filter(name=request.POST['unsubscribe']).first()
+            user_cabinet.subscriptions.remove(subscribe)
+            messages.success(request, "Unsubscribe was successful")
     return render(request, "main_app/categories.html", {
-        'categories': categories
+        'categories': categories,
+        'user_cabinet': user_cabinet
     })
 
 
