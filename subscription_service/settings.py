d1163214fc2cd323ac6554e1373e3bff297a8297
Andrii Tomalia <logotip123@yahoo.com>
2020-01-22 10:28:50 +0200

    adding postgres

--- subscription_service/settings.py
+++ subscription_service/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'subscription_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
